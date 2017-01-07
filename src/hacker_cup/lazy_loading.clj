917b7342fddf48b4ae20097324921865d0868274
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-01-08 00:48:10 +0100

    Rename function

--- src/hacker_cup/lazy_loading.clj
+++ src/hacker_cup/lazy_loading.clj
@@ -1,6 +1,6 @@
 (ns hacker-cup.lazy-loading)
 
-(defn fuckoff [in]
+(defn count-loads [in]
   (loop [q (reverse (sort in))
          stack [(first q)]
          carried 0]
@@ -10,13 +10,13 @@
       (if (>= (* (first stack) (count stack)) 50)
         (recur (rest q) [(second q)] (inc carried))
         (recur (drop-last q) (conj stack (last q)) carried)))))
-(fuckoff [30 30 1 1])
+(count-loads [30 30 1 1])
 ;; => 2
-(fuckoff [20 20 20])
+(count-loads [20 20 20])
 ;; => 1
-(fuckoff [1 2 3 4 5 6 7 8 9 10 11])
+(count-loads [1 2 3 4 5 6 7 8 9 10 11])
 ;; => 2
-(fuckoff [9 19 29 39 49 59])
+(count-loads [9 19 29 39 49 59])
 ;; => 3
-(fuckoff [32 56 76 8 44 60 47 85 71 91])
+(count-loads [32 56 76 8 44 60 47 85 71 91])
 ;; => 8
